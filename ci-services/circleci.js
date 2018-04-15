e1e41361b255e24771cbed83ed0cac6075e5de5e
Patrick Kubiak <patrick@antuple.net>
2018-04-15 19:17:03 -0400

    feat: circleci workflow support
    update on the first job of the first workflow or
    upload when not the first job in the workflow and the last commit is lockfile update

--- ci-services/circleci.js
+++ ci-services/circleci.js
@@ -1,13 +1,28 @@
 'use strict'
 
 const _ = require('lodash')
+const gitHelpers = require('../lib/git-helpers')
 
 const env = process.env
 
+/**
+ * Last commit is a lockfile update
+ */
+function isLockfileUpdate () {
+  const reUpdateLockfile = /^chore\(package\): update lockfile*$/mi
+  const lastCommitMessage = gitHelpers.getLastCommitMessage()
+  return reUpdateLockfile.test(lastCommitMessage)
+}
+
+// For use in workflows, the .git directory needs to be saved to cache after updating the lockfile
+// and the cache needs to be restored in a later job before uploading the lockfile
 module.exports = {
   repoSlug: `${env.CIRCLE_PROJECT_USERNAME}/${env.CIRCLE_PROJECT_REPONAME}`,
   branchName: env.CIRCLE_BRANCH,
-  firstPush: !env.CIRCLE_PREVIOUS_BUILD_NUM,
+  // CIRCLE_PREVIOUS_BUILD_NUM is null on the first job of the first workflow on the branch
+  // CIRCLE_WORKFLOW_UPSTREAM_JOB_IDS is null on the first job of every workflow on the branch
+  // (update on first job of first workflow) or (upload when not first job in workflow and last commit is lockfile update)
+  firstPush: !env.CIRCLE_PREVIOUS_BUILD_NUM || (env.CIRCLE_WORKFLOW_UPSTREAM_JOB_IDS && isLockfileUpdate()),
   correctBuild: _.isEmpty(env.CI_PULL_REQUEST),
   uploadBuild: env.CIRCLE_NODE_INDEX === `${env.BUILD_LEADER_ID || 0}`
 }
